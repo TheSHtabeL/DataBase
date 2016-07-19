ddc6b82f17bd70eaa3c137db229629203bcdbb8b
SHtabeL <theshtabel@gmail.com>
2016-07-19 16:53:33 +0300

    Добавлено: инициализация окна

--- src/MainMenu/GUI.java
+++ src/MainMenu/GUI.java
@@ -1,6 +1,19 @@
 package MainMenu;
 
+import org.lwjgl.*;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.*;
+import org.lwjgl.LWJGLException;
 
 public class GUI {
-
+    void init() {
+        try{
+            Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
+            Display.create();
+        }catch (LWJGLException e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
 }
--- src/MainMenu/control.java
+++ src/MainMenu/control.java
@@ -2,6 +2,7 @@ package MainMenu;
 
 public class control {
     public void main(){
-
+        GUI gui = new GUI();
+        gui.init();
     }
 }
