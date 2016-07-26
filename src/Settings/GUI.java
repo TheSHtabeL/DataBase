06dd2c846cc073a400ad0a0be44a5abca7a817a0
SHtabeL <theshtabel@gmail.com>
2016-07-26 16:13:44 +0300

    Добавлен класс подгрузки текстуры в программу

--- src/Settings/GUI.java
+++ src/Settings/GUI.java
@@ -1,11 +1,30 @@
 package Settings;
-
+import org.newdawn.slick.util.ResourceLoader;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.opengl.Texture;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
+
+import java.io.IOException;
 
 import static org.lwjgl.opengl.GL11.*;
 
 public class GUI {
+    private static Texture background;
+    void init(){
+        //Считываем текстуры
+        background = textureBind("background.png");
+    }
+    Texture textureBind(String name){
+        Texture texture = null;
+        try {
+            texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("img/Settings" + name));
+        } catch(IOException e){
+            e.printStackTrace();
+            System.out.print(1);
+        }
+        return texture;
+    }
     void clear(){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glColor3f(1.0f,1.0f,0.0f);
