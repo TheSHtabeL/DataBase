1b59043b1c93af87233a0b96b0ea2fadd85e092a
SHtabeL <theshtabel@gmail.com>
2016-07-31 19:40:10 +0300

    Добавлено: отрисовка динамических объектов сцены

--- src/MainScreen/GUI.java
+++ src/MainScreen/GUI.java
@@ -3,6 +3,7 @@ package MainScreen;
 import java.io.IOException;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.util.ResourceLoader;
@@ -13,6 +14,16 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class GUI {
     private static Texture background;
+    private static Texture naButton;
+    private static Texture view;
+    private static Texture aArrow;
+    private static Texture naArrow;
+    private static Texture aArrowReversed;
+    private static Texture naArrowReversed;
+    private static Texture aFind;
+    private static Texture naFind;
+    private static Texture aButton;
+    private static Button buttons[] = new Button[6];
 
     Texture textureBind(String name){
         Texture texture = null;
@@ -26,15 +37,26 @@ public class GUI {
 
     void init(){
         background = textureBind("background");
-
+        aButton = textureBind("green");
+        naButton = textureBind("red");
+        view = textureBind("view");
+        aArrow = textureBind("aArrow");
+        naArrow = textureBind("naArrow");
+        aArrowReversed = textureBind("aArrowReversed");
+        naArrowReversed = textureBind("naArrowReversed");
+        aFind = textureBind("aFind");
+        naFind = textureBind("naFind");
+        //Создаём кнопки
+        buttons[0] = new Button((Values.WIDTH/2)+350,(Values.HEIGHT/2)-150,(Values.WIDTH/2)+600,(Values.HEIGHT/2)-75);
+        buttons[1] = new Button((Values.WIDTH/2)+350,(Values.HEIGHT/2)-50,(Values.WIDTH/2)+600,(Values.HEIGHT/2)+25);
+        buttons[2] = new Button((Values.WIDTH/2)+350,(Values.HEIGHT/2)+50,(Values.WIDTH/2)+600,(Values.HEIGHT/2)+125);
+        buttons[3] = new Button((Values.WIDTH/2)-150,(Values.HEIGHT/2)+230,(Values.WIDTH/2)-50,(Values.HEIGHT/2)+330);
+        buttons[4] = new Button((Values.WIDTH/2)+50,(Values.HEIGHT/2)+230,(Values.WIDTH/2)+150,(Values.HEIGHT/2)+330);
+        buttons[5] = new Button((Values.WIDTH/2)-300,(Values.HEIGHT/2)-275,(Values.WIDTH/2)+300,(Values.HEIGHT/2)-205);
     }
     void setBackground(){
         //Натянуть текстуру background
         background.bind();
-        background.getImageHeight();
-        background.getImageWidth();
-        background.getTextureHeight();
-        background.getTextureWidth();
         glBegin(GL_QUADS);
         glTexCoord2f(0,0);
         glVertex2f(0,0);
@@ -48,6 +70,66 @@ public class GUI {
     }
     int draw(){
         setBackground();
+        //Отрисовка экрана вывода записей
+        view.bind();
+        glBegin(GL_QUADS);
+        glTexCoord2f(0,0);
+        glVertex2f((Values.WIDTH/2)-300, (Values.HEIGHT/2)-200);
+        glTexCoord2f(1,0);
+        glVertex2f((Values.WIDTH/2)+300, (Values.HEIGHT/2)-200);
+        glTexCoord2f(1,1);
+        glVertex2f((Values.WIDTH/2)+300, (Values.HEIGHT/2)+200);
+        glTexCoord2f(0,1);
+        glVertex2f((Values.WIDTH/2)-300, (Values.HEIGHT/2)+200);
+        glEnd();
+        //Выводим на экран кнопки
+        for(int i=0; i<buttons.length; i++){
+            if(buttons[i].isActive(Mouse.getX(),Values.HEIGHT-Mouse.getY())){
+                switch(i){
+                    case 0:
+                    case 1:
+                    case 2:
+                        aButton.bind();
+                        break;
+                    case 3:
+                        aArrowReversed.bind();
+                        break;
+                    case 4:
+                        aArrow.bind();
+                        break;
+                    case 5:
+                        aFind.bind();
+                        break;
+                }
+            }else{
+                switch (i) {
+                    case 0:
+                    case 1:
+                    case 2:
+                        naButton.bind();
+                        break;
+                    case 3:
+                        naArrowReversed.bind();
+                        break;
+                    case 4:
+                        naArrow.bind();
+                        break;
+                    case 5:
+                        naFind.bind();
+                        break;
+                }
+            }
+            glBegin(GL_QUADS);
+            glTexCoord2f(0,0);
+            glVertex2f(buttons[i].x0, buttons[i].y0);
+            glTexCoord2f(1,0);
+            glVertex2f(buttons[i].x1, buttons[i].y0);
+            glTexCoord2f(1,1);
+            glVertex2f(buttons[i].x1, buttons[i].y1);
+            glTexCoord2f(0,1);
+            glVertex2f(buttons[i].x0, buttons[i].y1);
+            glEnd();
+        }
         if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)){
             return 0;
         }else {
@@ -60,5 +142,12 @@ public class GUI {
     }
     void release(){
         background.release();
+        view.release();
+        aButton.release();
+        naButton.release();
+        aArrowReversed.release();
+        naArrowReversed.release();
+        aFind.release();
+        naFind.release();
     }
 }
