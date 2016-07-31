9fbe12db2eaf53d4a4c22e7364b9f3e9d66640a7
SHtabeL <theshtabel@gmail.com>
2016-07-31 17:32:14 +0300

    Добавлено: - метод считывания текстур в классе GUI - заглушки под другие методы

--- src/MainScreen/GUI.java
+++ src/MainScreen/GUI.java
@@ -1,7 +1,34 @@
 package MainScreen;
 
-/**
- * Created by SHtabeL on 31.07.2016.
- */
+
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+import java.io.IOException;
+
 public class GUI {
+    private static Texture background;
+
+    Texture textureBind(String name){
+        Texture texture = null;
+        try{
+            texture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("img/Main Screen/" + name + ".png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return texture;
+    }
+
+    void init(){
+        background = textureBind("background");
+
+    }
+    int draw(){
+
+        return 0;
+    }
+    void reveal(){
+        background.release();
+    }
 }
