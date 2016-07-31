6a198be74dbafeb3264929c855a496b6a60440b1
SHtabeL <theshtabel@gmail.com>
2016-07-31 15:23:31 +0300

    Добавлено: класс main теперь выполняет навигацию по коду

--- src/main.java
+++ src/main.java
@@ -1,8 +1,25 @@
 import MainMenu.*;
+import Settings.Control;
 
 public class main {
     public static void main(String[] args) {
-        MainMenu.control mainMenu = new control();
-        mainMenu.main();
+        MainMenu.Control mainMenu = new MainMenu.Control();
+        Settings.Control settings = new Settings.Control();
+        Init init = new Init();
+        init.init();
+        int flag = 0;
+        while(true) {
+            flag = mainMenu.main();
+            switch (flag){
+                case 0:
+                    break;
+                case 1:
+                    settings.main();
+                    break;
+                case 2:
+                default:
+                    return;
+            }
+        }
     }
 }
