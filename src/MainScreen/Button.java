8cddc9ad9ff2b4ace5ddf55fa8e547e5a2c6ae87
SHtabeL <theshtabel@gmail.com>
2016-07-31 17:19:21 +0300

    Добавлено: класс Button, отвечающий за кнопки

--- src/MainScreen/Button.java
+++ src/MainScreen/Button.java
@@ -1,7 +1,18 @@
 package MainScreen;
 
-/**
- * Created by SHtabeL on 31.07.2016.
- */
 public class Button {
+    int x0;
+    int x1;
+    int y0;
+    int y1;
+
+    Button(int x0,int y0,int x1,int y1) {
+        this.x0 = x0;
+        this.y0 = y0;
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+    boolean isActive(int mouseX,int mouseY){
+        return ( ( (mouseX>= x0) && (mouseX<=x1) ) && ( (mouseY>=y0) && (mouseY<=y1) ));
+    }
 }
