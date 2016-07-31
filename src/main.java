f6a162dc0112eb0ddf6b38a3d6842c2cf9b8026a
SHtabeL <theshtabel@gmail.com>
2016-07-31 17:51:08 +0300

    Добавлено: вызов основного экрана

--- src/main.java
+++ src/main.java
@@ -1,10 +1,12 @@
 import MainMenu.*;
 import Settings.Control;
+import MainScreen.*;
 
 public class main {
     public static void main(String[] args) {
         MainMenu.Control mainMenu = new MainMenu.Control();
         Settings.Control settings = new Settings.Control();
+        MainScreen.Control mainScreen = new MainScreen.Control();
         Init init = new Init();
         init.init();
         int flag = 0;
@@ -12,6 +14,7 @@ public class main {
             flag = mainMenu.main();
             switch (flag){
                 case 0:
+                    mainScreen.control();
                     break;
                 case 1:
                     settings.main();
