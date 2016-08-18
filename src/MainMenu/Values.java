a29570abd5f2c970089805dcf57520adde51fc85
TheSHtabeL <theshtabel@gmail.com>
2016-08-18 19:51:56 +0300

    Рефакторинг: избавляемся от старого кода для его редактирования

--- src/MainMenu/Values.java
+++
@@ -1,7 +0,0 @@
-package MainMenu;
-
-public class Values {
-    public static int WIDTH = 1280;
-    public static int HEIGHT = 720;
-    public static int FPS = 30;
-}
--- src/Settings/Values.java
+++
@@ -1,8 +0,0 @@
-package Settings;
-
-
-public class Values {
-    public static int FPS = 30;
-    public static int WIDTH = 1280;
-    public static int HEIGHT = 720;
-}
