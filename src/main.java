5bcd74ca1930814bafb8e0c6dd45fd8fb1bf4c92
TheSHtabeL <theshtabel@gmail.com>
2016-08-25 13:07:02 +0300

    Изменено: удалён устаревший код

--- src/main.java
+++ src/main.java
@@ -1,28 +1,6 @@
 import org.lwjgl.opengl.Display;
 
 public class main {
-//    public static void main(String[] args) {
-//        MainMenu.Control mainMenu = new MainMenu.Control();
-//        Settings.Control settings = new Settings.Control();
-//        MainScreen.Control mainScreen = new MainScreen.Control();
-//        Init init = new Init();
-//        init.init();
-//        int flag = 0;
-//        while(true) {
-//            flag = mainMenu.main();
-//            switch (flag){
-//                case 0:
-//                    mainScreen.control();
-//                    break;
-//                case 1:
-//                    settings.main();
-//                    break;
-//                case 2:
-//                default:
-//                    return;
-//            }
-//        }
-//    }
     public static void main(String[] args) {
         Control control = new ControlMainMenu();
         Init init = new Init();
