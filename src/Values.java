0ce0b6be4a29972d810e9ceaaeae1bf3514a326f
TheSHtabeL <theshtabel@gmail.com>
2016-08-25 12:34:43 +0300

    Добавлено: отслеживаемые значения для Caps Lock и текущего выбранного языка (en/ru)

--- src/Values.java
+++ src/Values.java
@@ -1,4 +1,6 @@
 public class Values {
+    public static boolean CapsLockState;
+    public static boolean enLocale = true;
     public static int FPS = 30;
     public static int WIDTH = 1280;
     public static int HEIGHT = 720;
