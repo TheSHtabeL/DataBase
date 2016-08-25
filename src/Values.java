6259d48897eb8e16c03c8d620779b3a41a240539
TheSHtabeL <theshtabel@gmail.com>
2016-08-25 13:07:38 +0300

    Изменено: Изначальное значение Caps Lock читается при инициализации класса

--- src/Values.java
+++ src/Values.java
@@ -1,5 +1,8 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Values {
-    public static boolean CapsLockState;
+    public static boolean CapsLockState = Toolkit.getDefaultToolkit().getLockingKeyState(KeyEvent.VK_CAPS_LOCK);
     public static boolean enLocale = true;
     public static int FPS = 30;
     public static int WIDTH = 1280;
