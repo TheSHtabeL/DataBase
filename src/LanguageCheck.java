febec386b5f6b3f772ac118b9480f1444a8fd8c3
TheSHtabeL <theshtabel@gmail.com>
2016-08-25 15:33:20 +0300

    Изменено: класс отредактировен для более корректной работы

--- src/LanguageCheck.java
+++ src/LanguageCheck.java
@@ -1,20 +1,25 @@
 import org.lwjgl.input.Keyboard;
 
 public class LanguageCheck {
-    private boolean permission = true;
+    private boolean localePermission = true;
+    private boolean capsPermission = true;
 
     public void updateLanguage(){
         //Класс отслеживает состояние управляющих клавиш для печати
-        while(Keyboard.next()){
-            if(Keyboard.getEventKeyState()){
-                if(Keyboard.getEventKey() == Keyboard.KEY_CAPITAL){
-                    Values.CapsLockState = !Values.CapsLockState; //Отслеживание состояние CapsLock
-                }else if( Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) & Keyboard.isKeyDown(Keyboard.KEY_LMENU) & permission){
-                    permission = false;
-                    Values.enLocale = !Values.enLocale; //Отслеживает переключение раскладки клавиатуры
-                }else{
-                    permission = true;
-                }
+        while(Keyboard.next()) {
+            if (capsPermission && Keyboard.isKeyDown(Keyboard.KEY_CAPITAL)) {
+                Values.CapsLockState = !Values.CapsLockState; //Отслеживание состояние CapsLock
+                System.out.println("Caps: " + Values.CapsLockState);
+                capsPermission = false;
+            } else {
+                capsPermission = true;
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) && Keyboard.isKeyDown(Keyboard.KEY_LMENU) && localePermission) {
+                localePermission = false;
+                Values.enLocale = !Values.enLocale; //Отслеживает переключение раскладки клавиатуры
+                System.out.println("Locale: " + Values.enLocale);
+            } else {
+                localePermission = true;
             }
         }
     }
