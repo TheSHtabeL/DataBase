4c3770616f19df5690d4bc2de824ac32dd69c95e
TheSHtabeL <theshtabel@gmail.com>
2016-09-01 14:35:04 +0300

    Добавлено: класс DataBase, который создаётся для подключения к базе данных посредством JDBC

---
+++ src/DataBase.java
@@ -0,0 +1,11 @@
+public class DataBase {
+    private String basename;
+
+    DataBase(){
+        basename = "test";
+    }
+    DataBase(String basename){
+        this.basename = basename;
+    }
+
+}
